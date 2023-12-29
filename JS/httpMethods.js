6b1f39825580a6826d243261a52ae74204d0b871
Namra-2303 <namrajoshiinventyv@gmail.com>
2023-12-29 23:41:37 +0530

    http_POST

--- JS/httpMethods.js
+++ JS/httpMethods.js
@@ -1,10 +1,5 @@
 // Function to perform a GET request
 
-// function getData(url) {
-//     return fetch(url)
-//         .then(response => response.json())
-//         .catch(error => console.error('Error:', error));
-// }
 async function getData(url) {
   try {
     const response = await fetch(url);
@@ -20,5 +15,72 @@ async function fetchData() {
   console.log(result);
 }
 
-fetchData();
+// fetchData();
+
+
+// Function to perform a POST request
+async function postData(url, body) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+
+// Function to perform a POST request
+let dataPosted = false;
+async function postData(url, body) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Example usage for POST request
+async function postNewClient() {
+  // Check if data has already been posted
+  if (!dataPosted) {
+    const newClientData = {
+      "isActive": true,
+      "age": 28,
+      "name": "John Doe",
+      "gender": "male",
+      "company": "ABC Corp",
+      "email": "johndoe@abc.com",
+      "phone": "+1 (123) 456-7890",
+      "address": "123 Main Street, Anytown, USA"
+    };
+
+    const postResult = await postData('http://localhost:3000/clients', newClientData);
+    console.log('POST Result:', postResult);
+
+    dataPosted = true;
+  } else {
+    console.log('Data has already been posted.');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // await postNewClient();
+});
+// fetchData();
   
