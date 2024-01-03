21c228ed9827ea50f66e000c09acefd3009f0d66
Namra-2303 <namrajoshiinventyv@gmail.com>
2024-01-03 18:56:07 +0530

    cominations_without_repetation

--- Pascal_Prime_Error/assets/main.js
+++ Pascal_Prime_Error/assets/main.js
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+function getCombinationsWithoutRepetition(number) {
+    const digits = number.toString().split('');
+    const result = [];
+
+    function combineWithoutRepetition(arr, length, current = []) {
+        if (current.length === length) {
+            result.push(parseInt(current.join('')));
+            return;
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            if (!current.includes(arr[i])) {
+                combineWithoutRepetition(arr, length, current.concat(arr[i]));
+            }
+        }
+    }
+
+    for (let len = 1; len <= digits.length; len++) {
+        combineWithoutRepetition(digits, len);
+    }
+
+    return result;
+}
+
+const inputNumber = 3451;
+const combinations = getCombinationsWithoutRepetition(inputNumber);
+console.log(combinations);
